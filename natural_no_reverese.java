c545dac09f70dc48374eb3e23a326a1a73bcd6d8
Vinay-Kumar-Pathak <vinaypathakvp31@gmail.com>
2025-12-27 20:07:06 +0530

    reverese the natural number

---
+++ natural_no_reverese.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class natural_no_reverese {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter the no n");
+        int n = sc.nextInt();
+        for(int i =n;i>=1;i--){
+            System.out.print(i+" ");
+            
+        }
+    }
+}
