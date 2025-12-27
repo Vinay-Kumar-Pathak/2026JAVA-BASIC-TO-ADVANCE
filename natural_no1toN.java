701365362336130ae0293a01cfe57384297b6521
Vinay-Kumar-Pathak <vinaypathakvp31@gmail.com>
2025-12-27 19:51:38 +0530

    natural no from 1 to n

---
+++ natural_no1toN.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class natural_no1toN{
+    public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+    int n= sc.nextInt();
+    System.out.print("natural number from 1 to N");
+        for(int i=0;i<=n;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
