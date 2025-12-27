1c93b61775f99cc8a91e21312495db698b9700fd
Vinay-Kumar-Pathak <vinaypathakvp31@gmail.com>
2025-12-27 21:10:43 +0530

    sum of even number

--- sumOFevenNO.java
+++ sumOFevenNO.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class sumOFevenNO {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n=
+         Scanner sc =new Scanner(System.in);
+        int n =sc.nextInt();
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            if (i%2==0){
+               System.out.print(i+"+");
+               sum+=i;
+            }
+            
+        }
+        
+    System.out.println("\nsum = " + sum);
+        sc.close();
     }
 }
