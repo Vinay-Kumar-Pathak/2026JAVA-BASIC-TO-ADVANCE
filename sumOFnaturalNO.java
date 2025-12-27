315743941a0f504309c66a919eb5be5e0c2ad7f2
Vinay-Kumar-Pathak <vinaypathakvp31@gmail.com>
2025-12-27 20:48:15 +0530

    sum of natural number

--- sumOFnaturalNO.java
+++ sumOFnaturalNO.java
@@ -4,6 +4,10 @@ public class sumOFnaturalNO {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int n =sc.nextInt();
-        for(int i=0;i<=n)
+        int sum=0;
+        for(int i=0;i<=n;i++){
+            sum+=i;
+        }
+        System.out.println(sum);
     }
 }
